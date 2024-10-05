ac698be3a541110a8c74db29f89e2ffa81e3ac5b
samueltingg <samuelting7@gmail.com>
2024-10-05 18:03:12 +0800

    feat: changed routing, joinClick redirects to vote/[id] instead of join/

--- frontend-next/src/app/home/page.tsx
+++ frontend-next/src/app/home/page.tsx
@@ -5,7 +5,8 @@ export default function Home() {
   const router = useRouter();
 
   const handleJoinClick = () => {
-    router.push(`/join`);
+    const id = 1;
+    router.push(`/vote/${id}`);
   };
 
   const handleCreateClick = () => {
@@ -14,16 +15,16 @@ export default function Home() {
 
   return (
     <div className="flex flex-row space-x-8">
-      
-    <button 
-      className="bg-blue-500 text-white text-lg font-bold py-4 px-8 rounded-lg hover:bg-blue-600 transition duration-300 text-center" 
+
+    <button
+      className="bg-blue-500 text-white text-lg font-bold py-4 px-8 rounded-lg hover:bg-blue-600 transition duration-300 text-center"
       type="button"
       onClick={handleJoinClick}>
       Join a group
     </button>
 
     <button
-      className="bg-green-500 text-white text-lg font-bold py-4 px-8 rounded-lg hover:bg-green-600 transition duration-300 text-center" 
+      className="bg-green-500 text-white text-lg font-bold py-4 px-8 rounded-lg hover:bg-green-600 transition duration-300 text-center"
       type="button"
       onClick={handleCreateClick}>
       Create a group
