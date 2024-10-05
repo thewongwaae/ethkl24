bb35b6985d0f79e8e860e4b8bf57cc2daeb99b25
Lewis lee <minecraftpe132006@gmail.com>
2024-10-06 01:42:33 +0800

    fix:

--- frontend-next/src/app/login/page.tsx
+++ frontend-next/src/app/login/page.tsx
@@ -1,6 +1,11 @@
+'use client'
+import { useWalletContext } from "@/components/wallet";
+
 export default function Login() {
+	const {walletAddress, connectWallet, disconnectWallet, contract} = useWalletContext(); 
   return (
     <div>
+			<button onClick={connectWallet}>hellow</button>
 		  login page
     </div>
   );
--- frontend-next/src/components/wallet.tsx
+++ frontend-next/src/components/wallet.tsx
@@ -1,24 +1,21 @@
 'use client'
-import React, { createContext, useState, useContext } from 'react';
+import React, { createContext, useState, useContext, useEffect } from 'react';
 import { ethers } from 'ethers';
 
 // Create the context
-const WalletContext = createContext();
+const WalletContext = createContext(null);
 
 const CONTRACT_ADDRESS = "0x5FbDB2315678afecb367f032d93F642f64180aa3"; // Deployed contract address
 // Create a provider component
 export const WalletProvider = ({ children }) => {
-  const [walletAddress, setWalletAddress] = useState(null);
-  const [signer, setSigner] = useState(null);
+	const [walletAddress, setWalletAddress] = useState(null);
 	const [contract, setContract] = useState<ethers.Contract | null>(null);
-	const [addressValue, setAddressValue] = useState<string | null>("");
 	const [provider, setProvider] = useState<ethers.providers.Web3Provider | null>(null);
 
 
-
-  // Function to connect the wallet
-  const connectWallet = async () => {
-   if (typeof window.ethereum !== "undefined") {
+	// Function to connect the wallet
+	const connectWallet = async () => {
+		if (typeof window.ethereum !== "undefined") {
 			try {
 
 				// Set up provider
@@ -44,23 +41,45 @@ export const WalletProvider = ({ children }) => {
 		} else {
 			console.log("MetaMask is not installed");
 		} 
-  };
+	};
 
-const initializeContract = async (provider, account) => {
+	const initializeContract = async (provider, account) => {
 		// console.log("provider", provider);
 		const signer = await provider.getSigner();
 		const contractInstance = new ethers.Contract(CONTRACT_ADDRESS, contractABI.abi, signer);
 		setContract(contractInstance);
 	};
 
-  return (
-    <WalletContext.Provider value={{ walletAddress, signer, connectWallet }}>
-      {children}
-    </WalletContext.Provider>
-  );
+	useEffect(() => {
+		if (window.ethereum) {
+			window.ethereum.on('accountsChanged', (accounts) => {
+				setWalletAddress(accounts[0]);
+				initializeContract(provider, accounts[0]);
+			});
+		}
+
+		return () => {
+			if (window.ethereum) {
+				window.ethereum.removeListener('accountsChanged', (accounts) => {
+					setWalletAddress(accounts[0]);
+					initializeContract(provider, accounts[0]);
+				});
+			}
+		};
+	}, []);
+
+	const disconnectWallet = () => {
+		setWalletAddress(null);
+	}
+
+	return (
+		<WalletContext.Provider value={{ walletAddress, connectWallet, disconnectWallet, contract }}>
+			{children}
+		</WalletContext.Provider>
+	);
 };
 
 // Custom hook to use the wallet context
-export const useWallet = () => {
+export const useWalletContext = () => {
   return useContext(WalletContext);
 };
